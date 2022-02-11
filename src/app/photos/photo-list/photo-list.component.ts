72f7773b02eb80021e4f3c7f835effadda075d0f
Hugo <hugohenrique20@gmail.com>
2022-02-10 22:22:11 -0300

    melhorando a performance da aplicação com subject do rxjs

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -8,31 +10,39 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(
     /*private photoService:PhotoService,*/
     private activedRoute: ActivatedRoute
     ) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-    this.photos = this.activedRoute.snapshot.data['photos'];
+      this.photos = this.activedRoute.snapshot.data['photos'];
+      this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
 
-    /* >>> NOTA: Agora o resolver está carregando as photos antes mesmo de o componente ser carregado,
-                 então o código abaixo é desnecessário, porém, sem o resolver, ele seria usado. <<<
+      /* >>> NOTA: Agora o resolver está carregando as photos antes mesmo de o componente ser carregado,
+      então o código abaixo é desnecessário, porém, sem o resolver, ele seria usado. <<<
 
-    const userName = this.activedRoute.snapshot.params.userName;//pegando a rota ativada no momento
-    this.photoService
-    .listFromUser(userName)//busca pelo usuário dinâmicamente
-    .subscribe(photos => this.photos = photos);*/
+      const userName = this.activedRoute.snapshot.params.userName;//pegando a rota ativada no momento
+      this.photoService
+      .listFromUser(userName)//busca pelo usuário dinâmicamente
+      .subscribe(photos => this.photos = photos);*/
+    }
+
+    /** Criei este método para resolver o problema de na tag html o target não conseguir obter o value */
+    onKeyUp(evento: KeyboardEvent) {
+      this.debounce.next((<HTMLInputElement>evento.target).value);
+    }
+
+    ngOnDestroy(): void {
+      this.debounce.unsubscribe();
+    }
   }
-
-  /** Criei este método para resolver o problema de na tag html o target não conseguir obter o value */
-  onKeyUp(evento: KeyboardEvent) {
-    this.filter = (<HTMLInputElement>evento.target).value;
-  }
-}
