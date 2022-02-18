e7dd7bd7799e2171be737a9222a868bb2b29fa67
Hugo <hugohenrique20@gmail.com>
2022-02-17 23:47:03 -0300

    criado página home com formulário de login com validação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 import { PhotosModule } from './photos/photos.module';
 
@@ -15,7 +16,8 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     AppRoutingModule,
     PhotosModule,
-    ErrorsModule
+    ErrorsModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,17 +1,10 @@
-import { PhotoFormModule } from './photo-form/photo-form.module';
-import { PhotoModule } from './photo/photo.module';
-import { PhotoListResolver } from './photo-list/photo-list.resolver';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosLayoutComponent } from './photo-list/photos-layout/photos-layout.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoModule } from './photo/photo.module';
 
 
 @NgModule({
