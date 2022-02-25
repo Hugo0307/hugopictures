d3c5f05f662d7e09b7c053f95fbe772c3c39ad65
Hugo Henrique <hugohenrique20@gmail.com>
2022-02-25 14:46:31 -0300

    acessando cabeçalho da resposta e pegando token

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -12,6 +13,10 @@ export class AuthService {
 
   /** Método que realiza a comunicação com a api do backend afim de validar os dados do usuário para login */
   authenticate(userName: string, password: string) {
-    return this.http.post(API_URL + '/user/login', { userName, password});
+    return this.http.post(API_URL + '/user/login', { userName, password }, { observe: 'response' })//a propridade observe deixa a resposta visível
+    .pipe(tap(resp => {
+      const authToken = resp.headers.get('x-access-token');//pegando o token no cabeçalho
+      console.log(authToken);
+    }))
   }
 }
