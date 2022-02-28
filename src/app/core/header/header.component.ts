9c1722e1b3e9e6b6a45524101d130a767c27f156
Hugo Henrique <hugohenrique20@gmail.com>
2022-02-27 21:36:07 -0300

    ao recarregar, o nome do usuário não some mais e logout implementado

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { UserService } from './../user/user.service';
 import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
@@ -12,11 +13,16 @@ import { User } from '../user/user';
 export class HeaderComponent {
 
   user$!: Observable<User>;
-  user!: User;
 
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private router: Router) {
 
     this.user$ = this.userService.getUser();
-    this.user$.subscribe(user => this.user = user);
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
   }
 }
--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,20 +1,18 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
 import { BehaviorSubject } from 'rxjs';
 
 import { TokenService } from './../token/token.service';
-import jwt_decode from 'jwt-decode';
 import { User } from './user';
 
 /** Serviço para armazenar o token com auxílio do TokenService e retornar o usuário logado*/
 @Injectable({ providedIn: 'root' })
-export class UserService implements OnInit {
+export class UserService {
 
   /** O Behavior Subject vai pegar a informação do usuário e guardar até que alguém utilize a informação */
   private userSubject = new BehaviorSubject<User>({id:0,name:'',email:''});
 
-  constructor(private tokenService: TokenService) { }
-
-  ngOnInit(): void {
+  constructor(private tokenService: TokenService) {
     this.tokenService.hasToken() &&
       this.decodeAndNotify();
   }
@@ -32,5 +30,10 @@ export class UserService implements OnInit {
     const token = this.tokenService.getToken();
     const user = jwt_decode(token) as User;
     this.userSubject.next(user);
+  }
+
+  logout() {
+    this.tokenService.removeToken();
+    this.userSubject.next({id:0,name:'',email:''});
   }
 }
