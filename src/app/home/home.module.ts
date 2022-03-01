02279b9fbec6f156450f83ea3695a644ba833c6c
Hugo Henrique <hugohenrique20@gmail.com>
2022-03-01 16:27:59 -0300

    implementando tela de registro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,23 @@
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup.component';
+
 
 @NgModule({
-  declarations: [ SignInComponent ],
+  declarations: [
+    SignInComponent,
+    SignUpComponent
+  ],
   imports: [
     ReactiveFormsModule,
     CommonModule,
     VMessageModule,
     RouterModule
-   ]
+  ]
 })
 export class HomeModule { }
