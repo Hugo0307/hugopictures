22bb97adba4ab3763083b4ea22e59a98ebf4325d
Hugo Henrique <hugohenrique20@gmail.com>
2022-03-03 20:47:21 -0300

    aplicando validações para tela de registro

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,6 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './signup.component.html'
 })
-export class SignUpComponent { }
+export class SignUpComponent implements OnInit{
+
+  singupForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this.singupForm = this.formBuilder.group({
+
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      fullName: ['', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(40)
+      ]],
+      userName: ['', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(30)
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-z0-9_\-]+$/),
+        Validators.minLength(8),
+        Validators.maxLength(14)
+      ]]
+    });
+  }
+}
